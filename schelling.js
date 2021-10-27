4719e240d7f24477e9f259f13738ac3b3568780b
Janett Ramirez <jramirezsevilla@pugetsound.edu>
2021-10-27 11:06:56 -0700

    Created a schelling's model class and created fields that will store the appropriate dimentions for the grid.

--- schelling.js
+++ schelling.js
@@ -1,4 +1,13 @@
 /**
  * Schelling's Model simulator
- * @author
+ * @author Janett
  */
+
+class schellingsModel() {
+    this.dimenstion = 50;
+    this.similarityThreshhold = 0.35;
+    this.vacantCells = 0.05;
+    this.popSplit = 0.5;
+    this.popOneColor = "";
+    this.popTwoColor = "";
+}
