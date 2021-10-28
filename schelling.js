7adc279b817e716b9bf5946dd0a5e1f1c6321537
Janett Ramirez <jramirezsevilla@pugetsound.edu>
2021-10-27 21:29:05 -0700

    Fixed code alignment.

--- schelling.js
+++ schelling.js
@@ -4,72 +4,74 @@
  */
 
 
-        //reads in the dimension input
-        let dimension = 50;
-        let input1 = document.querySelector("#dimension");
-        input1.addEventListener("input", () => {
-        dimension = input1.value;
-            console.log(dimension);
-            convertToTable();
+//reads in the dimension input
+let dimension = 50;
+let input1 = document.querySelector("#dimension");
+input1.addEventListener("input", () => {
+    dimension = input1.value;
+    console.log(dimension);
+    convertToTable();
 
-       });
+});
 
-       //reads in the similarity threshold input
-       let similarityThreshhold = 0.35;
-        let input2 = document.querySelector("#threshold");
-        input2.addEventListener("input", () => {
-            similarityThreshhold = input2.value;
-       });
+//reads in the similarity threshold input
+let similarityThreshhold = 0.35;
+let input2 = document.querySelector("#threshold");
+input2.addEventListener("input", () => {
+    similarityThreshhold = input2.value;
+});
 
-       //reads in the vacant cell input
-       let vacantCells = 0.05;
-        let input3 = document.querySelector("#vacantRatio");
-        input3.addEventListener("input", () => {
-            vacantCells = input3.value;
-       });
+//reads in the vacant cell input
+let vacantCells = 0.05;
+let input3 = document.querySelector("#vacantRatio");
+input3.addEventListener("input", () => {
+    vacantCells = input3.value;
+});
 
-       //reads in the population split input
-        let input4 = document.querySelector("#popRatio");
-        input4.addEventListener("input", () => {
-            this.popSplit = input4.value;
-       });
+//reads in the population split input
+let popSplit = 0.5;
+let input4 = document.querySelector("#popRatio");
+input4.addEventListener("input", () => {
+    popSplit = input4.value;
+});
 
-       //reads in the population X color
-        let input5 = document.querySelector("#popXcolor");
-        input5.addEventListener("input", () => {
-            this.popXColor = input5.value;
-       });
+//reads in the population X color
+let popXColor = "#006EFF";
+let input5 = document.querySelector("#popXcolor");
+input5.addEventListener("input", () => {
+    popXColor = input5.value;
+});
 
-        //reads in the population Y color
-        let input6 = document.querySelector("#popYcolor");
-        input6.addEventListener("input", () => {
-            this.popYwoColor = input6.value;
-       });
+//reads in the population Y color
+let popYColor
+let input6 = document.querySelector("#popYcolor");
+input6.addEventListener("input", () => {
+    popYColor = input6.value;
+});
 
-    let grid = new Array(dimension);
-    function convertToTable() {
-        //take from field once created
-        let table = document.querySelector("#board");
-        grid = new Array(dimension);
-        table.innerHTML = "";
-        for (let i = 0; i < dimension; i++) {
-            let tableRow = document.createElement("tr");
-            grid[i] = new Array(dimension);
-            for (let j = 0; j < dimension; j++) {
-                let tableData = document.createElement("td");
-                tableRow.appendChild(tableData);
-                grid[i][j] = tableData;
-            }
-            table.appendChild(tableRow);
+let grid = new Array(dimension);
+function convertToTable() {
+    let table = document.querySelector("#board");
+    grid = new Array(dimension);
+    table.innerHTML = "";
+    for (let i = 0; i < dimension; i++) {
+        let tableRow = document.createElement("tr");
+        grid[i] = new Array(dimension);
+        for (let j = 0; j < dimension; j++) {
+            let tableData = document.createElement("td");
+            tableRow.appendChild(tableData);
+            grid[i][j] = tableData;
         }
-        console.log(grid);
+        table.appendChild(tableRow);
     }
+    console.log(grid);
+}
 
     //math.floor to convert into int
 
 
 
-    convertToTable();
+convertToTable();
 
 //const tester = new schellingsModel();
 //tester.populateGrid();
